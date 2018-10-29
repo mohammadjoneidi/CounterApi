bab3d164f7731d5de3d714ea6cd9108861d7c73e
Mohammad Joneidi <joneidi.moh65@gmail.com>
2018-10-29 11:52:35 +0100

    create first test which return error

---
+++ CounterApi.Tests/ApiCounterTests/ApiCountUnitTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace CounterApi.Tests
+{
+    public class CounterServiceTest
+    {       
+
+        [Fact]
+        public void ShouldBeAbleToRetrieveTheCounter()
+        {
+           throw new NotImplementedException("Please create a test first");
+        }
+      
+    }
+}
